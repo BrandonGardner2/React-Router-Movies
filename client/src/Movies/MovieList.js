fa37042b35d6205b9b038d065442f67470e27f4e
Brandon G-Home <brandon.gardner2@outlook.com>
2019-02-25 14:28:56 -0600

    List routing functional

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -27,8 +27,8 @@ export default class MovieList extends Component {
     return (
       <div className="movie-list">
         {this.state.movies.map(movie => (
-          <Link to={`/${movie.id}`}>
-            <MovieCard key={movie.id} movie={movie} />
+          <Link key={movie.id} to={`/${movie.id}`}>
+            <MovieCard movie={movie} />
           </Link>
         ))}
       </div>
--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,22 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
-export default class SavedList extends Component {
-  constructor(props) {
-    super(props);
-  }
+const SavedList = props => {
+  return (
+    <div className="saved-list">
+      <h3>Saved Movies:</h3>
+      {props.list.map(movie => (
+        <span key={movie.id} className="saved-movie">
+          <Link to={`/${movie.id}`}>{movie.title}</Link>
+        </span>
+      ))}
+      <Link to="/" className="home-button">
+        Home
+      </Link>
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div className="saved-list">
-        <h3>Saved Movies:</h3>
-        {this.props.list.map(movie => (
-          <span className="saved-movie">{movie.title}</span>
-        ))}
-        <Link to="/" className="home-button">
-          Home
-        </Link>
-      </div>
-    );
-  }
-}
+export default SavedList;
